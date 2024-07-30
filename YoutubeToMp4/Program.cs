1b2bccfa0728b25ab3a6a988999bfaa96e7856f3
protogenposting <86360309+protogenposting@users.noreply.github.com>
2024-07-30 19:43:29 -0400

    the

--- YoutubeToMp4/Program.cs
+++ YoutubeToMp4/Program.cs
@@ -6,29 +6,43 @@ namespace YouTubeDownloader
     class Program
     {
         static YoutubeClient youtube = new YoutubeClient();
+        static List<string> exports = new List<string>{};
         static async Task Main(string[] args)
         {
             // Set the output directory path here
             string outputDirectory = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location)+"/videos";
+            var convert = new NReco.VideoConverter.FFMpegConverter();
             Directory.CreateDirectory(outputDirectory);
-
+            Console.WriteLine("Started Program");
             // List of YouTube video URLs to download
             List<string> videoUrls = new List<string>
             {
                 "https://youtu.be/dHlpa5utQzQ?si=p9dfdsyYug3quKux",
-                "https://youtu.be/weZKm1kTrpc?si=RQnT4j9LiauyfAUP",
+                /*"https://youtu.be/weZKm1kTrpc?si=RQnT4j9LiauyfAUP",
                 "https://youtu.be/kXMwZNRiPe0?si=QLE5J30SjXtyTwNS",
                 "https://youtu.be/9Zj0JOHJR-s?si=0_57adxXuFW4_0rx",
                 "https://youtu.be/EWjZOxs87yg?si=-TbodDxcSEStDB0w",
                 "https://youtu.be/rRzDNQPzbs4?si=kRRfgx_FQIhoWR_M",
-                "https://youtu.be/lCaun_EiJZQ?si=68xxVoxdNU3vgKH4"
+                "https://youtu.be/lCaun_EiJZQ?si=68xxVoxdNU3vgKH4"*/
                 // Add more video URLs as needed
             };
+            Console.WriteLine("Urls:");
+            foreach (var videoUrl in videoUrls)
+            {
+                Console.WriteLine(videoUrl);
+            }
             try
             {
                 foreach (var videoUrl in videoUrls)
                 {
                     await DownloadYouTubeVideo(videoUrl, outputDirectory);
+                }
+                foreach (var video in exports)
+                {
+                    convert.ConvertMedia(
+                        video,
+                        video+".mp3",
+                        "mp3");
                 }
             }
             catch (Exception ex)
@@ -44,6 +58,7 @@ namespace YouTubeDownloader
             // Sanitize the video title to remove invalid characters from the file name
             string sanitizedTitle = string.Join("_", video.Title.Split(Path.GetInvalidFileNameChars()));
 
+            Console.Write("started: "+sanitizedTitle);
             // Get all available muxed streams
             var streamManifest = await youtube.Videos.Streams.GetManifestAsync(video.Id);
             var muxedStreams = streamManifest.GetMuxedStreams().OrderByDescending(s => s.VideoQuality).ToList();
@@ -61,14 +76,8 @@ namespace YouTubeDownloader
 
                 Console.WriteLine("Download completed!");
                 Console.WriteLine($"Video saved as: {outputFilePath}{datetime}");
-                try{
-                    var convert = new NReco.VideoConverter.FFMpegConverter();
-                    convert.ConvertMedia(outputFilePath,outputFilePath+".mp3",".mp3");
-                }
-                catch(Exception e)
-                {
-                    Console.WriteLine(e.ToString());
-                }
+
+                exports.Add(outputFilePath);
             }
             else
             {
